434f60e6a8f170a8a356ef368a93f1a9c0c37eed
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-08-29 07:02:46 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now =new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('unable to append to server.log');
+    }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintainance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+//  res.send('<h1>Hello Express!</h1>');
+res.render('home.hbs',{
+  pageTitleHome: 'Welcome to our website',
+  headerTitle: 'Home Page'
+});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    headerTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Error has occured',
+    errorMessage: [
+      'Network not available',
+      'server connection failed'
+    ]
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on  port 3000');
+});
