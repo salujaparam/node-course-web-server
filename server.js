344187ed7e56541c59513f8c260de319dae5a5b3
salujaparam <41211906+salujaparam@users.noreply.github.com>
2018-08-29 18:49:41 +0530

    changes to the server.js and home.js

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintainance.hbs');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.render('maintainance.hbs');
+//   next();
+// });
 
 app.use(express.static(__dirname + '/public'));
 
